5d5c28efe8306e9efda0844f822b8101ce77249f
caio17ffn <flaviocaio40@gmail.com>
2026-05-20 08:24:01 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-@app.route('/operacao/<tipo>/<int:op1>/<int:op2>')
-def operacao(tipo, op1, op2):
-    if tipo == "sum":
-        return str(op1 + op2)
-    elif tipo == "sub":
-        return str(op1 - op2)
-    elif tipo == "mult":
-        return str(op1 * op2)
-    elif tipo == "div":
-        if op2 == 0:
-            return "Erro: divisão por zero"
-        return str(op1 / op2)
-    else:
-        return "Tipo inválido"
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def inicio():
+    return render_template('inicio.html')
+
+@app.route('/menu')
+def menu():
+
+    comidas = ['Pizza', 'Hamburguer', 'Batata']
+
+    return render_template('menu.html', comidas=comidas)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
+app.run(debug=True)
