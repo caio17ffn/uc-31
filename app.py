891b5605d879305533468b59c72ff02a51841544
caio17ffn <flaviocaio40@gmail.com>
2026-06-10 08:06:30 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,53 @@
-from flask import Flask, render_template
+ from flask import Flask, render_template, request, redirect, make_response
 
 app = Flask(__name__)
 
 @app.route('/')
 def inicio():
-    return render_template('inicio.html')
 
-@app.route('/menu')
-def menu():
+    nome = request.cookies.get('nome')
+    email = request.cookies.get('email')
+    tema = request.cookies.get('tema')
 
-    comidas = ['Pizza', 'Hamburguer', 'Batata']
+    if tema == None:
+        tema = 'claro'
 
-    return render_template('menu.html', comidas=comidas)
+    return render_template('inicio.html',
+                           nome=nome,
+                           email=email,
+                           tema=tema)
 
-@app.route('/contato')
-def contato():
-    return render_template('contato.html')
+
+@app.route('/salvar', methods=['POST'])
+def salvar():
+
+    nome = request.form['nome']
+    email = request.form['email']
+
+    resp = make_response(redirect('/'))
+
+    resp.set_cookie('nome', nome)
+    resp.set_cookie('email', email)
+
+    return resp
+
+
+@app.route('/claro')
+def claro():
+
+    resp = make_response(redirect('/'))
+    resp.set_cookie('tema', 'claro')
+
+    return resp
+
+
+@app.route('/escuro')
+def escuro():
+
+    resp = make_response(redirect('/'))
+    resp.set_cookie('tema', 'escuro')
+
+    return resp
+
 
 app.run(debug=True)
